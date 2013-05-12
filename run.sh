9625ad58da73a76efb5f8d019084180e7ab788a3
gary810410 <gary810410@gmail.com>
2013-05-12 19:20:09 +0800

    Images and .sh and source_file list

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+ARENA_CLASS=ntu.csie.oop13spring.Arena1
+PET1_CLASS=ntu.csie.oop13spring.PetSpider
+PET2_CLASS=ntu.csie.oop13spring.PetSpider
+PET3_CLASS=ntu.csie.oop13spring.PetMudMonster
+PET4_CLASS=ntu.csie.oop13spring.PetMudMonster
+PET5_CLASS=ntu.csie.oop13spring.PetSlime
+PET6_CLASS=ntu.csie.oop13spring.PetSlime
+PET7_CLASS=ntu.csie.oop13spring.PetSlime
+
+java -jar hw4.jar $ARENA_CLASS $PET1_CLASS $PET2_CLASS $PET3_CLASS $PET4_CLASS $PET6_CLASS $PET5_CLASS $PET7_CLASS
